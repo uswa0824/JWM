1710a09c4df80ee9f2ef19ba2d5e22fe1bfeed6b
Jeremiah Morris <uswa0824@msn.com>
2025-07-05 17:55:25 -0400

    Test Commit chnage

--- JWMiOS/ContentView.swift
+++ JWMiOS/ContentView.swift
@@ -1,4 +1,4 @@
-//
+
 //  ContentView.swift
 //  JWM
 //
@@ -18,9 +18,12 @@ struct ContentView: View {
             List {
                 ForEach(items) { item in
                     NavigationLink {
+
                         Text("Item at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
 
                         Text("Items at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
+
+                        Text("Itemss at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
 
                     } label: {
                         Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
